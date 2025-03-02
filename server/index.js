e180e01bc50a09f308e608e4575d2cd6742ceb7f
kawan <kawanchaudhary@gmail.com>
2025-03-02 10:35:48 +0530

    fix big

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 app.use(express.json());
