ca4cc2c08190d7b07dd2128779ed44870150c85d
kawan <kawanchaudhary@gmail.com>
2025-03-02 15:13:10 +0530

    finalized app

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 
+const activeQuizSchema = new mongoose.Schema({
+  score: { type: Number, default: 0 },
+  quizzes: [
+    {
+      clueUsed: { type: Boolean, default: false },
+      quizItem: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizItem' },
+      answer: { type: String, default: null },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+});
+
 const userSchema = new mongoose.Schema({
   userId: { type: String, default: uuidv4, unique: true },
   username: { type: String, required: true, unique: true },
@@ -14,18 +27,7 @@ const userSchema = new mongoose.Schema({
       score: { type: Number, required: true },
     },
   ],
-  activeQuiz: {
-    score: { type: Number, default: 0 },
-    quizzes: [
-      {
-        clueUsed: { type: Boolean, default: false },
-        quizItem: { type: mongoose.Schema.Types.ObjectId, ref: 'QuizItem' },
-        answer: { type: String, default: null },
-        createdAt: { type: Date, default: Date.now },
-      },
-    ],
-    createdAt: { type: Date, default: Date.now },
-  },
+  activeQuiz: { type: activeQuizSchema, default: null },
 });
 
 const User = mongoose.model('User', userSchema);
